28a01f5b481e6e048899b689311b4168a1970c45
Jairo <jairoccostajr@gmail.com>
2026-01-16 10:12:59 -0300

    630A.cpp

---
+++ 630A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio cin.tie(0); ios::sync_with_stdio(0)
+#define ll unsigned long long
+
+void solve(){
+    ll n; cin >> n;
+
+    cout << 25 << '\n';
+}
+
+signed main(){
+    fastio;
+    solve();
+}
