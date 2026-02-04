1e30298b4d68bdd77ad35204111c6558c77e756b
Jairo <jairoccostajr@gmail.com>
2026-02-04 10:53:52 -0300

    adds 2179A

---
+++ 2179A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio cin.tie(0); ios::sync_with_stdio(0)
+#define ll long long
+#define vs vector<string>
+#define vi vector<int>
+#define vll vector<long long>
+#define f(i) for(int i=0; i<(i); i++)
+
+void solve(){
+    int t; cin >> t;
+
+    while(t--){
+        int k, x; cin >> k >> x;
+        cout << k*x + 1 << '\n';
+    }
+}
+
+signed main(){
+    fastio;
+    solve();
+}
