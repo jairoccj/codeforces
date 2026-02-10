a7fa121e297c3cea3523faa17949d6d071fec354
Jairo <jairoccostajr@gmail.com>
2026-02-09 21:35:36 -0300

    adds 1703B

---
+++ 1703B.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio cin.tie(0); ios::sync_with_stdio(0)
+#define ll long long
+#define vs vector<string>
+#define vi vector<int>
+#define vll vector<long long>
+#define f(i) for(int i=0; i<(i); i++)
+
+
+void solve(){
+    int t; cin >> t;
+
+    while(t--){
+        int n; cin >> n;
+
+        string s; cin >> s;
+        set<int> mySet;
+        int sol = 0;
+
+        for(auto &x : s){
+            if(mySet.count(x) == 0){
+                sol += 2;
+                mySet.insert(x);
+            }
+            else sol++;
+        }
+
+        cout << sol << '\n';
+    }
+}
+
+signed main(){
+    fastio;
+    solve();
+}
