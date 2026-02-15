7cd75dc0ade557251e73c4d06812a5fc4febf71a
Jairo <jairoccostajr@gmail.com>
2026-02-15 12:46:45 -0300

    adds 1877A

---
+++ 1877A.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio cin.tie(0); ios::sync_with_stdio(0)
+#define ll long long
+#define vs vector<string>
+#define vi vector<int>
+#define vll vector<long long>
+#define f(i) for(int i=0; i<(i); i++)
+
+void solve(){
+    int t; cin >> t;
+
+    while(t--){
+        int n; cin >> n;
+        int sol = 0;
+
+        for(int i=0; i<n-1; i++){
+            int num; cin >> num;
+            sol += num;
+        }
+
+        cout << sol * -1 << '\n';
+    }
+}
+
+signed main(){
+    fastio;
+    solve();
+}
