6719418da3a0c9efaa96615b311d99db00b1388f
Jairo <jairoccostajr@gmail.com>
2026-03-11 20:37:34 -0300

    adds 1371A

---
+++ 1371A.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio cin.tie(0); ios::sync_with_stdio(0)
+#define ll unsigned long long
+#define vs vector<string>
+#define vi vector<int>
+#define vll vector<unsigned long long>
+
+void solve(){
+    int n; cin >> n;
+    
+    cout << (n+1)/2 << '\n';
+}
+
+signed main(){
+    fastio;
+    int t; cin >> t;
+    while(t--){
+        solve();
+    }
+}
