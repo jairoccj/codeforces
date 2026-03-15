d73029212b1f76a80a5243c8cdc0562ef55f5905
Jairo <jairoccostajr@gmail.com>
2026-03-15 12:55:07 -0300

    adds 1814A

---
+++ 1337A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int a, b, c, d; cin >> a >> b >> c >> d;
+
+    cout << b << ' ' << c << ' ' << c << '\n';
+}
+
+signed main() 
+{
+  int t; cin >> t;
+  while(t--)
+    solve();
+}
